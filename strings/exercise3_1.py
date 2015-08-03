94624f71c1536d5813f13f97490023082b285fe6
Anderson <andersonresende86@gmail.com>
2015-08-03 10:58:49 -0300

    Created function to_ping to be imported

--- strings/exercise3_1.py
+++ strings/exercise3_1.py
@@ -1,13 +1,19 @@
 #! coding: utf-8
 
-word = raw_input('Put the word: ')
-ping_word = ''
-if word[0] in 'aeiou':  # 1.1, 1.2, 1.3
-    ping_word = word + 'way'
-else:
-    sufix = word[0] + 'ay'
-    ping_word = word[1:] + sufix
-print ping_word
+
+def to_ping_latin(word):
+    ping_word = ''
+    if word[0] in 'aeiou':  # 1.1, 1.2, 1.3
+        ping_word = word + 'way'
+    else:
+        sufix = word[0] + 'ay'
+        ping_word = word[1:] + sufix
+    return ping_word
+
+
+if __name__ == '__main__':
+    word = raw_input('Put the word: ')
+    print to_ping_latin(word)
 
 
 ## 1.1 Usar apenas uma string ao invés de uma lista com as vogais
